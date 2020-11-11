34a52e04b673329d3cd0711f5f3aa40e8e8cd7fe
namnguyen <nguyenvannamptit@gmail.com>
2020-11-12 01:46:33 +0700

    update: student module

--- src/main/java/com/web/assgiment/dormitory/mapper/ObjectMapperUtils.java
+++ src/main/java/com/web/assgiment/dormitory/mapper/ObjectMapperUtils.java
@@ -9,31 +9,14 @@ import java.util.stream.Collectors;
 public class ObjectMapperUtils {
     private static ModelMapper modelMapper = new ModelMapper();
 
-    /**
-     * Model mapper property setting are specified in the following block.
-     * Default property matching strategy is set to Strict see {@link MatchingStrategies}
-     * Custom mappings are added using {@link ModelMapper#addMappings(PropertyMap)}
-     */
     static {
         modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
     }
 
-    /**
-     * Hide from public usage.
-     */
     private ObjectMapperUtils() {
     }
 
-    /**
-     * <p>Note: outClass object must have default constructor with no arguments</p>
-     *
-     * @param <D>      type of result object.
-     * @param <T>      type of source object to map from.
-     * @param entity   entity that needs to be mapped.
-     * @param outClass class of result object.
-     * @return new object of <code>outClass</code> type.
-     */
     public static <D, T> D toDto(final T entity, Class<D> outClass) {
         return modelMapper.map(entity, outClass);
     }
@@ -42,15 +25,7 @@ public class ObjectMapperUtils {
         return modelMapper.map(dto, outClass);
     }
 
-    /**
-     * <p>Note: outClass object must have default constructor with no arguments</p>
-     *
-     * @param entityList list of entities that needs to be mapped
-     * @param outCLass   class of result list element
-     * @param <D>        type of objects in result list
-     * @param <T>        type of entity in <code>entityList</code>
-     * @return list of mapped object with <code><D></code> type.
-     */
+
     public static <D, T> List<D> toDto(final Collection<T> entityList, Class<D> outCLass) {
         return entityList.stream()
                 .map(entity -> toDto(entity, outCLass))
@@ -63,12 +38,6 @@ public class ObjectMapperUtils {
                 .collect(Collectors.toList());
     }
 
-    /**
-     * Maps {@code source} to {@code destination}.
-     *
-     * @param source      object to map from
-     * @param destination object to map to
-     */
     public static <S, D> D map(final S source, D destination) {
         modelMapper.map(source, destination);
         return destination;
